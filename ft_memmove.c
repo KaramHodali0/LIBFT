336c7c278574ce951f97fe5286b809ac8b941d91
kalhouda <kalhouda@c1r4s9.irbid.42.school>
2026-09-13 14:12:22 +0300

    fix memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: kalhouda <kalhouda@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/10 11:57:29 by kalhouda          #+#    #+#             */
-/*   Updated: 2026/09/12 14:43:04 by kalhouda         ###   ########.fr       */
+/*   Updated: 2026/09/13 14:09:26 by kalhouda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	s = (const unsigned char *)src;
 	if (d > src)
 	{
-		while (--n > 0)
+		while (n-- > 0)
 			d[n] = s[n];
 	}
 	else
